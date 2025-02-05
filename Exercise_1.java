b31e46773a00df8ef7bb7be78292b4549de47e54
Ananya Pawar <98231622+ananyapawar601@users.noreply.github.com>
2025-02-05 01:43:29 -0500

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,13 +1,35 @@
+// Time Complexity : O(logn)
+// Space Complexity :   O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : None 
+
+
+// Your code here along with comments explaining your approach
+
 class BinarySearch { 
-    // Returns index of x if it is present in arr[l.. r], else return -1 
-    int binarySearch(int arr[], int l, int r, int x) 
-    { 
-        //Write your code here
+    // Returns index of x if it is present in arr[l..r], else return -1 
+    int binarySearch(int arr[], int l, int r, int x) { 
+        if (l <= r) {
+            int mid = l + (r - l) / 2; // Prevents integer overflow
+
+            // Check if x is at mid
+            if (arr[mid] == x)
+                return mid;
+
+            // If x is smaller, search the left subarray
+            if (arr[mid] > x)
+                return binarySearch(arr, l, mid - 1, x);
+
+            // Otherwise, search the right subarray
+            return binarySearch(arr, mid + 1, r, x);
+        }
+
+        // Element is not present in the array
+        return -1;
     } 
-  
-    // Driver method to test above 
-    public static void main(String args[]) 
-    { 
+
+    // Driver method to test the above 
+    public static void main(String args[]) { 
         BinarySearch ob = new BinarySearch(); 
         int arr[] = { 2, 3, 4, 10, 40 }; 
         int n = arr.length; 
@@ -18,4 +40,4 @@ class BinarySearch {
         else
             System.out.println("Element found at index " + result); 
     } 
-} 
+}  
